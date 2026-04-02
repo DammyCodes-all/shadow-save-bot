0d79a4883e5b77ba3d4803d4254d4bc86613af09
Dammycodes-all <harliarmeen@gmail.com>
2026-04-02 16:14:26 +0100

    feat: update greeting message in AppService to provide bot usage instructions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to Shadow Save Bot! \n\nThis bot allows you to download TikTok videos without watermarks. \n\n To gets started access the bot from https://t.me/shadow_save_bot and send a TikTok video link. \n\nExample:\nhttps://vm.tiktok.com/XXXXXXX/';
   }
 }
