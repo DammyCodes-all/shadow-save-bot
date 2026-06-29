b394e9fd1bfc7e84917a665d44b3a265c9cc5973
Dammycodes-all <harliarmeen@gmail.com>
2026-06-29 05:30:49 +0100

    feat: add TypeORM for Neon Postgres and UserModule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
 import { DownloadModule } from './download/download.module';
+import { UserModule } from './user/user.module';
 import { validateEnvironment } from './config/env.validation';
 
 @Module({
@@ -12,6 +14,27 @@ import { validateEnvironment } from './config/env.validation';
       isGlobal: true,
       validate: validateEnvironment,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        extra: {
+          ssl: { rejectUnauthorized: false },
+          connectionTimeoutMillis: 30000,
+          idleTimeoutMillis: 60000,
+          keepAlive: true,
+          keepAliveInitialDelayMillis: 10000,
+          max: 3,
+        },
+        maxQueryExecutionTime: 10000,
+        retryDelay: 3000,
+        retryAttempts: 3,
+      }),
+    }),
+    UserModule,
     BotModule,
     DownloadModule,
   ],
